2065f211dc21cbbe3fa1518b9f923fa49b2598ed
Shinji Takae <shnjtk@gmail.com>
2018-01-20 12:19:35 +0900

    [Add] install and setup prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,23 @@ module.exports = {
     es6: true
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:flowtype/recommended'
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:flowtype/recommended"
   ],
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
       experimentalObjectRestSpread: true
     }
   },
-  plugins: [
-    'react',
-    'flowtype'
-  ],
+  plugins: ["react", "flowtype", "prettier"],
+  rules: {
+    "prettier/prettier": ["error", { singleQuote: true }]
+  },
   settings: {
     flowtype: {
       onlyFilesWithFlowAnnotation: true
